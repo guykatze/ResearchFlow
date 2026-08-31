a464c5dfe6ddbf39fa9880cf795f6db9866f0deb
guykatze <guykatze@gmail.com>
2026-08-31 08:59:32 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 
-const CACHE = "researchflow-v0.4.4";
+const CACHE = "researchflow-v0.4.5";
 const CORE = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app-v0.4.4.js",
+  "./app-v0.4.5.js",
   "./manifest.webmanifest",
   "./icons/icon-180.png",
   "./icons/icon-192.png",
